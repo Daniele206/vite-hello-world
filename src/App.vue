49dff0e82c74979c14b25fb9aa7e0c2aa6a99f38
Daniele206 <danieledeieso2004@gmail.com>
2024-03-20 14:49:02 +0100

    [add] content to page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default{
+    data(){
+      return{
+        title: 'Hello word',
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi et suscipit laborum blanditiis tempore vel repellat dolorum, adipisci atque accusamus consectetur quae reprehenderit magni esse! Atque magnam aliquid iste dolore?'
+      }
+    }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="page">
+    <div class="text">
+      <h1 class="fw-bold main-title">{{ title }}</h1>
+      <p class="text-center fs-3">{{ text }}</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h1{
+  color: yellow;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+p{
+  color:red;
 }
 </style>
